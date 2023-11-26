c9520074868e5b84589d41136b98cf2d743ebef9
Tahir Zahid <devtahirzahid@gmail.com>
2023-11-27 00:21:15 +0500

    edit contact details to original

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -9,14 +9,13 @@ const Footer = () => {
         backgroundColor: '#f0f0f0',
         padding: '20px',
         textAlign: 'center',
-        mt: 'auto', // This ensures the footer stays at the bottom of the page
+        mt: 'auto',
       }}
     >
       <Container>
         <Typography variant='h6'>Contact Information</Typography>
-        <Typography variant='body1'>Email: your@email.com</Typography>
-        <Typography variant='body1'>Phone: +123 456 7890</Typography>
-        {/* Add any other contact information or links you want */}
+        <Typography variant='body1'>Email: devtahirzahid@gmail.com</Typography>
+        <Typography variant='body1'>Phone: +92 322 4969</Typography>
       </Container>
     </Box>
   );
