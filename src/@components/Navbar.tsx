01aeca6a97e732bc10215867483d13d7032f9f7c
Tahir Zahid <devtahirzahid@gmail.com>
2023-11-26 20:59:16 +0500

    modified with mui

--- src/@components/Navbar.tsx
+++ src/@components/Navbar.tsx
@@ -1,103 +1,142 @@
-'use client'; // this is a client component
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-scroll/modules';
 import { usePathname } from 'next/navigation';
 import { useTheme } from 'next-themes';
 import { RiMoonFill, RiSunLine } from 'react-icons/ri';
-import { IoMdMenu, IoMdClose } from 'react-icons/io';
 
-interface NavItem {
-  label: string;
-  page: string;
-}
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { Grid } from '@mui/material';
 
-const NAV_ITEMS: Array<NavItem> = [
-  {
-    label: 'Home',
-    page: 'home',
-  },
-  {
-    label: 'About',
-    page: 'about',
-  },
-  {
-    label: 'Projects',
-    page: 'projects',
-  },
-];
+const pages = ['Home', 'About', 'Projects'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
   const pathname = usePathname();
   const [navbar, setNavbar] = useState(false);
   return (
-    <header className='w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600'>
-      <div className='justify-between md:items-center md:flex'>
-        <div>
-          <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-            <Link to='home'>
-              <div className='container flex items-center space-x-2'>
-                <h2 className='text-2xl font-bold'>Tahir Rajpoot</h2>
-              </div>
-            </Link>
-            <div className='md:hidden'>
-              <button
-                className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border'
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? 'block' : 'hidden'
-            }`}
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Typography
+            variant='h6'
+            noWrap
+            component='a'
+            href='#app-bar-with-responsive-menu'
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
           >
-            <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
-              {NAV_ITEMS.map((item, idx) => {
-                return (
-                  <Link
-                    key={idx}
-                    to={item.page}
-                    className={
-                      'block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100'
-                    }
-                    activeClass='active'
-                    spy={true}
-                    smooth={true}
-                    offset={-100}
-                    duration={500}
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    {item.label}
-                  </Link>
-                );
-              })}
+            Tahir
+          </Typography>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Grid
+            container
+            spacing={4}
+            display={'flex'}
+            justifyContent={'end'}
+            alignItems={'center'}
+          >
+            <Grid item>
               {currentTheme === 'dark' ? (
-                <button
-                  onClick={() => setTheme('light')}
-                  className='bg-slate-100 p-2 rounded-xl'
-                >
-                  <RiSunLine size={25} color='black' />
-                </button>
+                <Tooltip title='Switch Mode'>
+                  <IconButton onClick={() => setTheme('light')} sx={{ p: 0 }}>
+                    <RiSunLine size={25} color='black' />
+                  </IconButton>
+                </Tooltip>
               ) : (
-                <button
-                  onClick={() => setTheme('dark')}
-                  className='bg-slate-100 p-2 rounded-xl'
-                >
-                  <RiMoonFill size={25} />
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+                <Tooltip title='Switch Mode'>
+                  <IconButton onClick={() => setTheme('dark')} sx={{ p: 0 }}>
+                    <RiMoonFill size={25} />
+                  </IconButton>
+                </Tooltip>
+              )}{' '}
+            </Grid>
+            <Grid item>
+              <Tooltip title='Open settings'>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id='menu-appbar'
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign='center'>{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
