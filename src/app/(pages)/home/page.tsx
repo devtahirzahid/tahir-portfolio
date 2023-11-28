7571040ef0d0c92f3f8be633f671719c412f4728
Tahir Zahid <devtahirzahid@gmail.com>
2023-11-28 23:25:21 +0500

    changes file

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -10,7 +10,7 @@ import FeaturedPost from '@components/@components/dashboard/FeaturedPost';
 import Sidebar from '@components/@components/dashboard/Sidebar';
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'Title of a longer featured Blog Post',
   description:
     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
   image: 'https://source.unsplash.com/random?wallpapers',
