6461837b49a6fd678252d1283d6c50f2ea523854
Tahir Zahid <devtahirzahid@gmail.com>
2023-11-30 00:13:11 +0500

    little fix

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -12,7 +12,7 @@ import Sidebar from '@components/@components/dashboard/Sidebar';
 const mainFeaturedPost = {
   title: 'Title of a longer featured Blog Post',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    "Multiple lines of text gfhjhgfghjhgfgx that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
   image: 'https://source.unsplash.com/random?wallpapers',
   imageText: 'main image description',
   linkText: 'Continue reading…',
