b36488b6f0fdfda6f52737ecc23d5769c713d97f
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-01 00:11:55 +0500

    added pending text

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 export default function About() {
   return (
     <Box>
-      Lets start the about page design thjats all about that
+      pending Lets start the about page design thjats all about that
       <Typography variant='h1'>ala re ala</Typography>
     </Box>
   );
