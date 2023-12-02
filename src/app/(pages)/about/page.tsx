779754ee95f0f64c50d0e55536bbf849c2f3bfdd
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-02 21:52:23 +0500

    updated text in main page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,7 +3,8 @@ import { Box, Typography } from '@mui/material';
 export default function About() {
   return (
     <Box>
-      pending Lets start the about page design thjats all about that
+      pending that all the backlog Lets start the about page design thjats all
+      about that
       <Typography variant='h1'>ala re ala</Typography>
     </Box>
   );
