df3e95002e24e66df86be49edf9e2bf0d6bde455
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-02 21:55:38 +0500

    started page again from scratch

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -3,8 +3,7 @@ import { Box, Typography } from '@mui/material';
 export default function About() {
   return (
     <Box>
-      pending that all the backlog Lets start the about page design thjats all
-      about that
+      Lets start designing this page from scratch{' '}
       <Typography variant='h1'>ala re ala</Typography>
     </Box>
   );
