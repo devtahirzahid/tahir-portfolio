fc329c83aded6aab2053cce161de8f6700038e0b
Tahir Zahid <142656236+devtahirzahid@users.noreply.github.com>
2023-12-04 15:38:47 +0500

    Update page.tsx o about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -8,10 +8,10 @@ export default function About() {
         <Typography variant='body1'>Sample Description </Typography>
       </Grid>
       <Grid item xs={6} md={6} lg={6}>
-        <Typography variant='body2'>Sample Description </Typography>
+        <Typography variant='body2'>Sample sub Description 1 </Typography>
       </Grid>
       <Grid item xs={6} md={6} lg={6}>
-        <Typography variant='body2'>Sample Description </Typography>
+        <Typography variant='body2'>Sample sub Description 2 </Typography>
       </Grid>
     </Grid>
   );
