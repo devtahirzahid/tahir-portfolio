6aab57d91df60fb84df19eab5397da727f32c55f
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-05 22:01:56 +0500

    updated and spike for the about page

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -5,7 +5,9 @@ export default function About() {
     <Grid container padding={'1rem'}>
       Lets start designing this page from scratch{' '}
       <Grid item xs={12} md={12} lg={12}>
-        <Typography variant='body1'>Sample Description </Typography>
+        <Typography variant='body1'>
+          Sample Description Lets thing about the design
+        </Typography>
       </Grid>
       <Grid item xs={6} md={6} lg={6}>
         <Typography variant='body2'>Sample sub Description 1 </Typography>
