d6eb319ae3fa47a2e29a5fea9b7af7d2687a7365
Tahir Zahid <devtahirzahid@gmail.com>
2023-12-15 00:00:32 +0500

    modified about page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Rebuild this component
-  // Rebuild this component
-  // Rebuild this component
   // Using sneat configs // help from errands
   return (
     <html lang='en'>
